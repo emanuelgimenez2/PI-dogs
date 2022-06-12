b709783fe429aa6c2d2a1eebf92d0474d7e5583e
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2022-06-11 22:37:12 -0300

    pagination, img 404,

---
+++ client/src/actions/actions.js
@@ -0,0 +1,11 @@
+export const GET_DOGS = "GET_DOGS";
+export const GET_DETAIL = "GET_DETAIL";
+export const GET_TEMPERAMENTS = "GET_TEMPERAMENTS";
+export const GET_DOG_NAME = "GET_DOG_NAME";
+export const POST_DOG = "POST_DOG";
+export const ORDER_BY_ALPHABETICAL = "ORDER_BY_ALPHABETICAL";
+export const ORDER_BY_WEIGHT = "ORDER_BY_WEIGHT";
+export const FILTER_DOGS_BY_TEMPERAMENT = "FILTER_DOGS_BY_TEMPERAMENT";
+export const FILTER_BY_CREATED = "FILTER_BY_CREATED";
+export const CLEAR_STATE = "CLEAR_STATE";
+export const ORDER = "ORDER";
