06e44ce7064cb883aa2bdc879b1db7924e2158c6
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2022-09-03 21:42:39 -0300

    deploy back

--- api/src/db.js
+++ api/src/db.js
@@ -2,9 +2,32 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,NODE_ENV } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+
+let sequelize =
+process.env.NODE_ENV === "production"
+? new Sequelize({
+  database: DB_NAME,
+  dialect: "postgres",
+  host: DB_HOST,
+  port: 5432,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  pool:{
+    max:3,
+    min:0,
+    idle:10000
+  },
+  dialectOptions: {
+    ssl:{
+      require:true,
+      rejectUnaauthorized:false
+    },
+    keepAlive: true,
+  },
+  ssl: true,
+}):new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
